982662d7446c2f354600582e001fa7782dab77bb
Fred Chasen <fchasen@gmail.com>
2018-03-19 16:25:34 -0700

    Add example project, tidy up

---
+++ StaticServerExample/App.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native Static Server
+ * https://github.com/futurepress/react-native-static-server
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  WebView,
+  Image,
+} from 'react-native';
+
+import StaticServer from 'react-native-static-server';
+import RNFS from 'react-native-fs';
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(opts) {
+    super();
+
+    this.state = {
+      origin: ''
+    }
+  }
+
+  componentWillMount() {
+    this.port = this.props.port || 0;
+    this.root = this.props.root || "www/";
+    this.file = this.props.file || './index.html';
+
+    // Get HTML file from require
+    let html = require('./index.html');
+    let {uri} = Image.resolveAssetSource(html);
+
+    let path = RNFS.DocumentDirectoryPath + '/www';
+
+    // Add the directory
+    RNFS.mkdir(path, { NSURLIsExcludedFromBackupKey: true });
+
+    // Fetch the file
+    let download = RNFS.downloadFile({
+      fromUrl: uri,
+      toFile: path + "/index.html"
+    })
+
+    download.promise.then(() => {
+      // Create a StaticServer at a random port
+      this.server = new StaticServer(this.port, this.root, {localOnly: true});
+
+      return this.server.start().then((origin) => {
+        this.setState({origin});
+      });
+    })
+
+  }
+
+  componentWillUnmount() {
+    if (this.server) {
+      this.server.kill();
+    }
+  }
+
+  render() {
+
+    if (!this.state.origin) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    return (
+      <WebView
+        source={{uri: `${this.state.origin}/index.html`}}
+        style={styles.webview}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  webview: {
+    marginTop: 20,
+    flex: 1,
+  }
+});
