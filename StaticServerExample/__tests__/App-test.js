fd81ea474bf73ad0cbd27068d6519955aa3d388f
Taro Matsuzawa <taro@georepublic.co.jp>
2020-04-17 19:43:37 +0900

    WIP supports Typescript, drops Component

--- StaticServerExample/__tests__/App-test.js
+++ StaticServerExample/__tests__/App-test.js
@@ -2,13 +2,14 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
+// eslint-disable-next-line no-undef
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
