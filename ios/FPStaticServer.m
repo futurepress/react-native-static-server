ad834880226e0ba2b6a42a84ba63a0e8f4bdff45
Ben Pham <ben.pham@pointsbet.com>
2022-08-15 19:06:16 +1000

    commit

--- ios/FPStaticServer.m
+++ ios/FPStaticServer.m
@@ -92,23 +92,23 @@ RCT_EXPORT_METHOD(start: (NSString *)port
         GCDWebServerResponse* response = nil;
         NSString* filePath = [directoryPath stringByAppendingPathComponent:GCDWebServerNormalizePath([request.path substringFromIndex:basePath.length])];
         NSString* fileType = [[[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:NULL] fileType];
-        if (fileType) {
+         if (fileType) {
           if ([fileType isEqualToString:NSFileTypeDirectory]) {
             if (indexFilename) {
               NSString* indexPath = [filePath stringByAppendingPathComponent:indexFilename];
               NSString* indexType = [[[NSFileManager defaultManager] attributesOfItemAtPath:indexPath error:NULL] fileType];
               if ([indexType isEqualToString:NSFileTypeRegular]) {
-                      response = [[GCDWebServerFileResponse alloc] initWithFile:indexPath byteRange:NSMakeRange(NSUIntegerMax, 0) isAttachment:NO mimeTypeOverrides:@{@"xml" : @"application/xml"}];
+                response = [GCDWebServerFileResponse responseWithFile:indexPath];
               }
             } else {
               response = [GCDWebServerResponse responseWithStatusCode:kGCDWebServerHTTPStatusCode_NotFound];
             }
           } else if ([fileType isEqualToString:NSFileTypeRegular]) {
             if (allowRangeRequests) {
-             response = [GCDWebServerFileResponse responseWithFile:filePath byteRange:request.byteRange]; ;
-              [response setValue:@"bytes" forAdditionalHeader:@"Accept-Rang"];
+              response = [GCDWebServerFileResponse responseWithFile:filePath byteRange:request.byteRange];
+              [response setValue:@"bytes" forAdditionalHeader:@"Accept-Ranges"];
             } else {
-                response = [GCDWebServerFileResponse responseWithFile:filePath];
+              response = [GCDWebServerFileResponse responseWithFile:filePath];
             }
           }
         }
