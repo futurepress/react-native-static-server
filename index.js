511f0650cdfd5f88edef594524a4380786ff3c06
Ben Pham <ben.pham@pointsbet.com>
2022-08-18 22:54:37 +1000

    fix syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {
 	NativeModules,
 	AppState,
 	Platform
- } from 'react-native';
+} from 'react-native';
 
 const { FPStaticServer } = NativeModules;
 
@@ -18,30 +18,35 @@ class StaticServer {
 				this.root = root || ROOT;
 				this.localOnly = (opts && opts.localOnly) || false;
 				this.keepAlive = (opts && opts.keepAlive) || false;
+				this.mimeTypeOverrides = opts.mimeTypeOverrides || null;
 				break;
 			case 2:
 				this.port = `${port}`;
-				if (typeof(arguments[1]) === 'string') {
+				if (typeof (arguments[1]) === 'string') {
 					this.root = root;
 					this.localOnly = false;
 					this.keepAlive = false;
+					this.mimeTypeOverrides = null;
 				} else {
 					this.root = ROOT;
 					this.localOnly = (arguments[1] && arguments[1].localOnly) || false;
 					this.keepAlive = (arguments[1] && arguments[1].keepAlive) || false;
+					this.mimeTypeOverrides = arguments[1].mimeTypeOverrides || null;
 				}
 				break;
 			case 1:
-				if (typeof(arguments[0]) === 'number') {
+				if (typeof (arguments[0]) === 'number') {
 					this.port = `${port}`;
 					this.root = ROOT;
 					this.localOnly = false;
 					this.keepAlive = false;
+					this.mimeTypeOverrides = null;
 				} else {
 					this.port = PORT;
 					this.root = ROOT;
 					this.localOnly = (arguments[0] && arguments[0].localOnly) || false;
 					this.keepAlive = (arguments[0] && arguments[0].keepAlive) || false;
+					this.mimeTypeOverrides = arguments[0].mimeTypeOverrides || null;
 				}
 				break;
 			default:
@@ -49,6 +54,7 @@ class StaticServer {
 				this.root = ROOT;
 				this.localOnly = false;
 				this.keepAlive = false;
+				this.mimeTypeOverrides = null;
 		}
 
 
@@ -58,7 +64,7 @@ class StaticServer {
 	}
 
 	start() {
-		if( this.running ){
+		if (this.running) {
 			return Promise.resolve(this.origin);
 		}
 
@@ -69,7 +75,7 @@ class StaticServer {
 			AppState.addEventListener('change', this._handleAppStateChangeFn);
 		}
 
-		return FPStaticServer.start(this.port, this.root, this.localOnly, this.keepAlive)
+		return FPStaticServer.start(this.port, this.root, this.localOnly, this.keepAlive, this.mimeTypeOverrides)
 			.then((origin) => {
 				this._origin = origin;
 				return origin;
