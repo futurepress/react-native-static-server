dd0ce8bc48c6e41ce494b97700215f956c4501e5
Ben Pham <ben.pham@pointsbet.com>
2022-08-18 22:55:01 +1000

    fix syntax

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,24 @@
 declare module 'react-native-static-server' {
   type Options = {
-    localOnly?: boolean;
-    keepAlive?: boolean;
-    mimeTypeOverrides?: Record<string, string>;
+    localOnly?: boolean
+    keepAlive?: boolean
+    mimeTypeOverrides?: Record<string, string>
   };
 
   export default class StaticServer {
-    constructor(port: number, root?: string, opts?: Options,);
+    constructor(port: number, root?: string, opts?: Options)
 
-    port: number;
-    root: string;
-    localOnly: boolean;
-    keepAlive: boolean;
-    started: boolean;
-    _origin?: string;
-    mimeTypeOverrides: Record<string, string>;
+    port: number
+    root: string
+    localOnly: boolean
+    keepAlive: boolean
+    started: boolean
+    _origin?: string
+    mimeTypeOverrides: Record<string, string>
 
-    start: () => Promise<string>;
-    stop: () => Promise<any>;
-    isRunning: () => Promise<boolean>;
-    kill: () => void;
+    start: () => Promise<string>
+    stop: () => Promise<any>
+    isRunning: () => Promise<boolean>
+    kill: () => void
   }
 }
