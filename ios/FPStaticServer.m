c98c2ac54dd491b4901189c337b2d5aa3813d004
Ben Pham <ben.pham@pointsbet.com>
2022-08-18 11:16:18 +1000

    commit

--- ios/FPStaticServer.m
+++ ios/FPStaticServer.m
@@ -34,7 +34,7 @@ RCT_EXPORT_METHOD(start:(NSString *)port
                   root:(NSString *)optroot
                   localOnly:(BOOL *)localhost_only
                   keepAlive:(BOOL *)keep_alive
-                  overrides:(NSDictionary<NSString*, NSString*> *)mimeType
+                  overrides:(NSDictionary<NSString*, NSString*>*)mimeType
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
 
@@ -76,13 +76,14 @@ RCT_EXPORT_METHOD(start:(NSString *)port
     
     self.mimeType = mimeType;
 
-    // if(!self.mimeType) {
-    //     mimeType = nil;
-    // }
+    if(!self.mimeType) {
+        mimeType = nil;
+    }
     
 
 
     //[_webServer addGETHandlerForBasePath:@"/" directoryPath:self.www_root indexFilename:@"index.html" cacheAge:3600 allowRangeRequests:YES];
+    NSDictionary<NSString*, NSString*> *overrides = self.mimeType
     NSString *basePath = @"/";
     NSString *directoryPath = self.www_root;
     NSString *indexFilename = @"index.html";
@@ -114,10 +115,10 @@ RCT_EXPORT_METHOD(start:(NSString *)port
             }
           } else if ([fileType isEqualToString:NSFileTypeRegular]) {
           if (allowRangeRequests) {
-              response = [[GCDWebServerFileResponse alloc] initWithFile:filePath byteRange:request.byteRange isAttachment:NO mimeTypeOverrides:mimeType] ;
+              response = [[GCDWebServerFileResponse alloc] initWithFile:filePath byteRange:request.byteRange isAttachment:NO mimeTypeOverrides:overrides] ;
               [response setValue:@"bytes" forAdditionalHeader:@"Accept-Ranges"];
             } else {
-              response = [[GCDWebServerFileResponse alloc] initWithFile:filePath byteRange:NSMakeRange(NSUIntegerMax, 0) isAttachment:NO mimeTypeOverrides:mimeType];
+              response = [[GCDWebServerFileResponse alloc] initWithFile:filePath byteRange:NSMakeRange(NSUIntegerMax, 0) isAttachment:NO mimeTypeOverrides:overrides];
             }
           }
         }
